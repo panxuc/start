152c74ed717d8fb772fc9c1244295c4e9eb06bc5
Xuc Pan <panxuc@panxuc.com>
2024-10-20 23:38:45 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import React from "react";
-import Head from "next/head";
 import Footer from "./components/Footer";
 import SearchDiv from "./components/SearchDiv";
 import TabSwitcher from "./components/TabSwitcher";
@@ -10,14 +9,6 @@ import Title from "./components/Title";
 export default function Home() {
   return (
     <>
-
-      <Head>
-        <script
-          defer
-          src="https://analytics.panxuc.com/script.js"
-          data-website-id="a853dab6-8e8d-4c85-95c2-0d48bb54e7e7"
-        ></script>
-      </Head>
 
       <main className="flex min-h-screen flex-col items-center p-10">
 
@@ -28,6 +19,12 @@ export default function Home() {
         <TabSwitcher />
 
         <Footer />
+
+        <script
+          defer
+          src="https://analytics.panxuc.com/script.js"
+          data-website-id="a853dab6-8e8d-4c85-95c2-0d48bb54e7e7"
+        ></script>
 
       </main>
 
