24605456f28208f90db00e766d181307b58392d0
Xuc Pan <panxuc@panxuc.com>
2025-04-07 23:43:03 +0800

    Update ExtraScript.tsx

--- app/components/ExtraScript.tsx
+++ app/components/ExtraScript.tsx
@@ -4,7 +4,7 @@ const ExtraScript: React.FC = () => {
     return (
         <>
             <script defer src="https://analytics.panxuc.com/script.js" data-website-id="a853dab6-8e8d-4c85-95c2-0d48bb54e7e7" />
-            <script src="https://cdn.jsdelivr.net/gh/panxuc/live2d-widget-mygo@latest/autoload.js" async />
+            <script src="https://live2d-widget-mygo.panxuc.com/autoload.js" async />
         </>
     );
 };
